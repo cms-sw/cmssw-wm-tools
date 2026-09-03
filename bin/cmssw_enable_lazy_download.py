dfbc6ef10cf02f799e3f9f93b5a96ed3084f6934
Norraphat <srimanob@mail.cern.ch>
2026-09-03 11:19:31 +0200

    Only require LHESource for lazy-download when --check_lhe_workflow is set (pLHE workflow)

--- bin/cmssw_enable_lazy_download.py
+++ bin/cmssw_enable_lazy_download.py
@@ -21,17 +21,31 @@ import re
 
 from tweak_program_helpers import make_parser, do_loop, get_cmssw_version, isCMSSWSupported
 
-
+def get_source_type(process):
+    inputSource = getattr(process, "source", None)
+    if inputSource is None:
+        return None
+    if hasattr(inputSource, "type_"):
+        return inputSource.type_()
+    if hasattr(inputSource, "_TypedParameterizable__type"):
+        return inputSource._TypedParameterizable__type
+    return None
 
 def handle_lazy(process, args):
-
-   process.add_(cms.Service("SiteLocalConfigService",
-                            overrideSourceCacheHintDir=cms.untracked.string("lazy-download")))
-   print("Added lazy-download to SIteLocalConfigService")
-   return process
+    if getattr(args, "check_lhe_workflow", False):
+        source_type = get_source_type(process)
+        if source_type != "LHESource":
+            return process
+    
+    process.add_(cms.Service("SiteLocalConfigService",
+                             overrideSourceCacheHintDir=cms.untracked.string("lazy-download")))
+    print("Added lazy-download to SIteLocalConfigService")
+    return process
 
 def init_argparse():
     parser = make_parser("Enable the lazy download service")
+    parser.add_argument("--check_lhe_workflow", action="store_true",
+                         help="For LHE workflow, only enable lazy-download if process.source is LHESource")
     return parser
 
 
